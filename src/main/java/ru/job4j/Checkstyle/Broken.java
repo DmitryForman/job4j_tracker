21d597f54df65c493b39cb3f56e8a539de308fa7
DmitryForman <masterdemion@gmail.com>
2023-04-07 16:50:46 +0300

    Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/Checkstyle/Broken.java
@@ -0,0 +1,28 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    private String surname;
+    private String name;
+
+    public void echo() {
+    }
+
+    public Broken() {
+
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+    public static final String NEW_VALUE = "";
+
+
+}

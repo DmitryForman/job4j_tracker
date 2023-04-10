93becab482b13ad64f41b7f7cdcc63ed1fa69dc4
DmitryForman <masterdemion@gmail.com>
2023-04-10 15:38:00 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String formatCreated = item.getCreated().format(formatter);
-        System.out.println(formatCreated);
+        Item item = new Item(1, "bug");
+        System.out.println(item);
     }
+
 }

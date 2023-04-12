fd19beafae06130e8012ddf00034b669e1ba4d3d
DmitryForman <masterdemion@gmail.com>
2023-04-12 08:59:29 +0300

    upd

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,17 +2,16 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (i == key) {
+            if (key.equals(i)) {
                 return i;
             }
-        } throw new ElementNotFoundException("");
+        } return throw new ElementNotFoundException("");
     }
     public static void main(String[] args) {
         try {
-            load("jdbc://localhost:8080");
-        } catch (UserInputException e) {
+            indexOf(-1);
+        } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
     }
